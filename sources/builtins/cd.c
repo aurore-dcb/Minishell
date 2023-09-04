0ad6df0979948a250166a29165b0dc97be7a1067
Aurore DUCOBU <aducobu@paul-f5Ar1s1.clusters.42paris.fr>
2023-09-04 09:12:00 +0200

    builtins

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,22 +6,12 @@
 /*   By: aducobu <aducobu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 13:04:51 by aducobu           #+#    #+#             */
-/*   Updated: 2023/09/01 14:19:48 by aducobu          ###   ########.fr       */
+/*   Updated: 2023/09/01 14:26:16 by aducobu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 #include <dirent.h>
-
-// modifier le PATH ?
-// void builtin_cd(char *chemin)
-// {
-//     if (chdir(chemin) == -1)
-//     {
-//         perror("Error ");
-//         return ;
-//     }
-// }
 
 char	*find_path_ret(t_env *envp, char *str)
 {
--- sources/builtins/env.c
+++ sources/builtins/env.c
@@ -6,25 +6,11 @@
 /*   By: aducobu <aducobu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 12:55:30 by aducobu           #+#    #+#             */
-/*   Updated: 2023/09/01 14:09:37 by aducobu          ###   ########.fr       */
+/*   Updated: 2023/09/01 14:26:34 by aducobu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
-
-// void builtin_env(char **env)
-// {
-//     int i;
-
-//     i = 0;
-// 	if (!env)
-// 		return ;
-// 	while (env[i])
-// 	{
-// 		printf("%s\n", env[i]);
-// 		i++;
-// 	}
-// }
 
 void	builtin_env(s_data *data)
 {
